311930aadca854a38405991d3e969e8b06249e94
Seungjun <rkdtmdwns0105@gmail.com>
2025-07-06 19:52:43 +0900

    baekjoon: 9498

---
+++ 0x02/0x02_9498.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int score;
+    cin >> score;
+
+    if (score >= 90) cout << "A" << "\n";
+    else if (score >= 80) cout << "B" << "\n";
+    else if (score >= 70) cout << "C" << "\n";
+    else if (score >= 60) cout << "D" << "\n";
+    else cout << "F";
+}
