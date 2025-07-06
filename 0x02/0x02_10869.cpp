6e28d071973498334e0845541cec6fe3bb289f9c
Seungjun <rkdtmdwns0105@gmail.com>
2025-07-06 19:42:32 +0900

    baekjoon: 10869

---
+++ 0x02/0x02_10869.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main () {
+    int A, B;
+    cin >> A >> B;
+
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    cout << A % B << "\n";
+}
